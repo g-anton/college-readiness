bac3485a9fc339da90c539fab4031fea8df148aa
g-anton <113036633+g-anton@users.noreply.github.com>
2022-11-04 17:54:50 -0400

    Updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+let counter = 0;
 
-console.log("Hello, world!");
+const goForward = () => {
+  if (counter < 4) {
+    counter++;
+    let idToMatch = "#slide-" + counter;
+    document.querySelector(idToMatch).classList.remove("hidden"); 
+  }
+};
+
+const goBack = () => {
+  if (counter > 0) {
+    let idToMatch= "#slide-" + counter;
+    document.querySelector(idToMatch).classList.add("hidden");
+    counter--;
+  }
+};
+
+const downloadPNG = () => {
+  let containerToSave = document.querySelector("#checklist");
+  
+  html2canvas(containerToSave, {}).then(
+    (canvas) => {
+      let a = document.createElement("a");
+      a.download = "checklist.png";
+      a.href = canvas.toDataURL("image/png");
+      a.click();
+    }
+  )
+};
+
+const openChecklist = () => {
+  let checklist = document.querySelector("#checklist");
+  checklist.classList.remove("noheight");
+  checklist.classList.add("fullheight");
+}
